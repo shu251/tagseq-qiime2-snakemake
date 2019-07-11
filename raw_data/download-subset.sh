8b07583bd46b983ab1df2b1e5468b6099b3649e5
shu251 <sarah.hu@whoi.edu>
2019-07-11 15:42:24 -0400

    download option

---
+++ raw_data/download-subset.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR580/008/SRR5808588/SRR5808588_1.fastq.gz -o SRR5808588_Diel_18S_survey_SCOPE_cruise_July_2015_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR580/008/SRR5808588/SRR5808588_2.fastq.gz -o SRR5808588_Diel_18S_survey_SCOPE_cruise_July_2015_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR580/004/SRR5808584/SRR5808584_1.fastq.gz -o SRR5808584_Diel_18S_survey_SCOPE_cruise_July_2015_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR580/004/SRR5808584/SRR5808584_2.fastq.gz -o SRR5808584_Diel_18S_survey_SCOPE_cruise_July_2015_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR580/002/SRR5808602/SRR5808602_1.fastq.gz -o SRR5808602_Diel_18S_survey_SCOPE_cruise_July_2015_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR580/002/SRR5808602/SRR5808602_2.fastq.gz -o SRR5808602_Diel_18S_survey_SCOPE_cruise_July_2015_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR580/002/SRR5808582/SRR5808582_1.fastq.gz -o SRR5808582_Diel_18S_survey_SCOPE_cruise_July_2015_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR580/002/SRR5808582/SRR5808582_2.fastq.gz -o SRR5808582_Diel_18S_survey_SCOPE_cruise_July_2015_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR580/009/SRR5808589/SRR5808589_1.fastq.gz -o SRR5808589_Diel_18S_survey_SCOPE_cruise_July_2015_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR580/009/SRR5808589/SRR5808589_2.fastq.gz -o SRR5808589_Diel_18S_survey_SCOPE_cruise_July_2015_2.fastq.gz
