3da5d6567d5807df35e05f02f711cc052ea40de8
shu251 <sarah.hu@whoi.edu>
2020-01-01 18:58:04 -0500

    revised snakemake asv script

--- submitscripts/submit-slurm-OTU.sh
+++ submitscripts/submit-slurm-OTU.sh
@@ -2,5 +2,5 @@
 
 snakemake   \
         --jobs 100 -s Snakefile-otu \
-        --use-conda --cluster-config submitscripts/cluster.yaml --cluster "sbatch --parsable --partition={cluster.queue} --job-name=otu.{rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes} --verbose"
+        --use-conda --cluster-config submitscripts/cluster.yaml --cluster "sbatch --parsable --partition={cluster.queue} --job-name=otu.{rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}"
 
