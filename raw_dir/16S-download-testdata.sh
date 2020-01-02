41a4b803584cb92bbd820c41cfe6f4e62e7806eb
shu251 <sarah.hu@whoi.edu>
2020-01-02 11:22:52 -0500

    uploaded 16S test data

---
+++ raw_dir/16S-download-testdata.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR855/009/SRR8559039/SRR8559039_1.fastq.gz -o SRR8559039_ALOHA_Deep_Trap_2014_14_16S_DNA_Amplicons_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR855/009/SRR8559039/SRR8559039_2.fastq.gz -o SRR8559039_ALOHA_Deep_Trap_2014_14_16S_DNA_Amplicons_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR855/002/SRR8559042/SRR8559042_1.fastq.gz -o SRR8559042_ALOHA_Deep_Trap_2014_12_16S_DNA_Amplicons_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR855/002/SRR8559042/SRR8559042_2.fastq.gz -o SRR8559042_ALOHA_Deep_Trap_2014_12_16S_DNA_Amplicons_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR855/001/SRR8559041/SRR8559041_1.fastq.gz -o SRR8559041_ALOHA_Deep_Trap_2014_13_16S_DNA_Amplicons_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR855/001/SRR8559041/SRR8559041_2.fastq.gz -o SRR8559041_ALOHA_Deep_Trap_2014_13_16S_DNA_Amplicons_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR855/008/SRR8559038/SRR8559038_1.fastq.gz -o SRR8559038_ALOHA_Deep_Trap_2014_01_16S_DNA_Amplicons_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR855/008/SRR8559038/SRR8559038_2.fastq.gz -o SRR8559038_ALOHA_Deep_Trap_2014_01_16S_DNA_Amplicons_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR855/003/SRR8559043/SRR8559043_1.fastq.gz -o SRR8559043_ALOHA_Deep_Trap_2014_21_16S_DNA_Amplicons_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR855/003/SRR8559043/SRR8559043_2.fastq.gz -o SRR8559043_ALOHA_Deep_Trap_2014_21_16S_DNA_Amplicons_2.fastq.gz
