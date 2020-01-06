8da8711e6689899b2229f7a18d3a406db7c7e881
shu251 <sarah.hu@whoi.edu>
2020-01-06 10:36:18 -0500

    annotated R script

--- scripts/write-manifest-current.R
+++ scripts/write-manifest-current.R
@@ -13,6 +13,8 @@ paths$PATH <- path_to_files
 
 # Extract sample ID
 paths_run <- paths %>% mutate(SAMPLEID = str_replace(FASTQ, "(\\w*?)_(\\w+).fastq.gz","\\1"))
+## ^See wildcard options on this line to modify how R script pulls out your sample IDs from fastq files
+
 paths_run$SAMPLEID <- gsub("./","", paths_run$SAMPLEID)
 paths_run$FASTQ <- gsub("./","", paths_run$FASTQ)
 
@@ -24,7 +26,8 @@ manifest_orig <- data.frame(paths_run$SAMPLEID, paths_run$FULL_PATH)
 colnames(manifest_orig)[1:2]<-c("sample-id","absolute-filepath")
 
 # If R1_001.fastq.gz or _1.fastq.gz ending, fill new column with forward, else reverse
-manifest_orig$direction <- ifelse(grepl("R1_001.fastq.gz|_1.fastq.gz", manifest_orig$`absolute-filepath`), "forward", "reverse")
+manifest_orig$direction <- ifelse(grepl("R1_001.fastq.gz|_1.fastq.gz|R1.fastq.gz", manifest_orig$`absolute-filepath`), "forward", "reverse")
+## ^Works with these options as file name suffix (R1_001.fastq.gz|_1.fastq.gz|R1.fastq.gz), modify if you have something else
 
 # Write output as a manifest file
 write.table(manifest_orig, "manifest-orig.txt",quote=FALSE,col.names=TRUE,row.names=FALSE,sep=",")
